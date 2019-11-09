dfa87c796f94e53d1652f93da327dcd6d0f69eb0
Illkwon Ha <haik0925@gmail.com>
2019-11-09 09:51:00 -0800

    making player jump

--- Assets/Scripts/StickMan.cs
+++ Assets/Scripts/StickMan.cs
@@ -2,11 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody2D))]
 public class StickMan : MonoBehaviour
 {
-    public float Speed = 2f;
+    public float MovementSpeed = 2f;
+    public float JumpSpeed = 2f;
 
     private Rigidbody2D _rigidbody;
+    private bool _isGrounded = false;
 
     void Start()
     {
@@ -14,15 +17,35 @@ public class StickMan : MonoBehaviour
         _rigidbody.freezeRotation = true;
     }
 
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("haha");
+        if (collision.gameObject.tag == "Ground" & _isGrounded == false)
+            _isGrounded = true;
+    }
+
+    void OnCollisionStay2D(Collision2D collision)
+    {
+        Debug.Log("haha2");
+        if (collision.gameObject.tag == "Ground" & _isGrounded == false)
+            _isGrounded = true;
+    }
+
     void Update()
     {
         if (Input.GetKey(KeyCode.RightArrow))
         {
-            _rigidbody.velocity = Vector2.right * Speed;
+            _rigidbody.velocity = Vector2.right * MovementSpeed;
         }
         if (Input.GetKey(KeyCode.LeftArrow))
         {
-            _rigidbody.velocity = Vector2.left * Speed;
+            _rigidbody.velocity = Vector2.left * MovementSpeed;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space) && _isGrounded)
+        {
+            _rigidbody.AddForce(new Vector2(0, 2) * JumpSpeed, ForceMode2D.Impulse);
+            _isGrounded = false;
         }
     }
 }
