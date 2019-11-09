b0a0d7d878b429e5c5ded6842362b9a171840174
Illkwon Ha <haik0925@gmail.com>
2019-11-09 09:31:35 -0800

    basic player movement

---
+++ Assets/Scripts/StickMan.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StickMan : MonoBehaviour
+{
+    public float Speed = 2f;
+
+    private Rigidbody2D _rigidbody;
+
+    void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+        _rigidbody.freezeRotation = true;
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            _rigidbody.velocity = Vector2.right * Speed;
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            _rigidbody.velocity = Vector2.left * Speed;
+        }
+    }
+}
