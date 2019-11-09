9e8364c0884dc88401e6cc0ff0130323ceef7dd3
Illkwon Ha <haik0925@gmail.com>
2019-11-09 08:50:08 -0800

    added icon & implementing icon bullet

---
+++ Assets/Scripts/IconBullet.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IconBullet : MonoBehaviour
+{
+    public enum State
+    {
+        Idle,
+        Bullet
+    }
+
+    public float DegreesPerSec = 360f;
+    public float Speed = 10f;
+
+    private Vector2 _velocity;
+    private State _state = State.Bullet;
+    private float _t = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //var playerPos = new Vector2();
+        _velocity = Vector2.down * Speed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_state == State.Bullet)
+        {
+            _t += Time.deltaTime;
+            transform.rotation = Quaternion.Euler(0, 0, _t * DegreesPerSec);
+        }
+    }
+}
