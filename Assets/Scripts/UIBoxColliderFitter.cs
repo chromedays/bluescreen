8726846cab7160bf70e9e6681e11976468760640
Illkwon Ha <haik0925@gmail.com>
2019-11-09 15:52:34 -0800

    added UIBoxColliderFitter

---
+++ Assets/Scripts/UIBoxColliderFitter.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider2D), typeof(RectTransform))]
+public class UIBoxColliderFitter : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var rect = GetComponent<RectTransform>();
+        var collider = GetComponent<BoxCollider2D>();
+        collider.size = rect.rect.size;
+        collider.offset = new Vector2(
+            (0.5f - rect.pivot.x) * rect.rect.width,
+            (0.5f - rect.pivot.y) * rect.rect.height);
+    }
+}
