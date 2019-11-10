ed2d79f8c5c1a67b659eb9201fc0f2839b5c17e8
Illkwon Ha <haik0925@gmail.com>
2019-11-09 16:15:23 -0800

    added global Game class

---
+++ Assets/Scripts/Game.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+public class Game : MonoBehaviour
+{
+    public static Game Inst = null;
+
+    public StickMan StickMan = null;
+
+    void Awake()
+    {
+        Assert.IsNull(Inst);
+        Inst = this;
+    }
+}
