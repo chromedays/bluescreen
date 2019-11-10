782ccbf25ba205d1f0b3f06e28cc622cb0a442f3
Illkwon Ha <haik0925@gmail.com>
2019-11-09 19:21:03 -0800

    added hp bar to player

---
+++ Assets/Scripts/StickManLifeBar.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StickManLifeBar : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Game.Inst.StickManLifeBar = this;
+    }
+
+    // Update is called once per frame
+    void PreUpdate()
+    {
+        transform.position = Game.Inst.StickMan.transform.position + new Vector3(0, 0.5f, 0);
+    }
+}
