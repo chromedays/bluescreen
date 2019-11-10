5300b6570bd7011bd7eae6ae157eac4b0270c029
Illkwon Ha <haik0925@gmail.com>
2019-11-09 22:30:28 -0800

    added simple windows hp manager

---
+++ Assets/Scripts/WindowsHP.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.Assertions;
+
+public class WindowsHP : MonoBehaviour
+{
+    public int MaxLifeCount = 100;
+
+    private int _lifeCount;
+
+    void Start()
+    {
+        _lifeCount = MaxLifeCount;
+
+        Assert.IsNull(Game.Inst.WindowsHP);
+        Game.Inst.WindowsHP = this;
+    }
+
+    void ReduceLife()
+    {
+        --_lifeCount;
+    }
+}
